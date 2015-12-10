da3d25db4dc6f724574857a6d833be728c12ee91
Rasika Gayan <rasikagayangunarathna@gmail.com>
2015-12-09 23:29:56 -0800

    solve the action bar conflict

--- app/src/main/java/com/rasikagayan/android/viewpagerexample/DataListActivity.java
+++ app/src/main/java/com/rasikagayan/android/viewpagerexample/DataListActivity.java
@@ -1,30 +1,9 @@
 package com.rasikagayan.android.viewpagerexample;
 
-import android.app.Activity;
 import android.app.Fragment;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 
 public class DataListActivity extends SingleFragmentActivity {
 
-//    @Override
-//    protected void onCreate(Bundle savedInstanceState) {
-//        super.onCreate(savedInstanceState);
-//        setContentView(R.layout.activity_fragment);
-//        Toolbar myToolbar = (Toolbar) findViewById(R.id.action_bar_default);
-//        setSupportActionBar(myToolbar);
-//
-//
-//    }
-
-
-    //@Override
-    //protected void onCreate(Bundle savedInstanceState) {
-        //super.onCreate(savedInstanceState);
-        //Toolbar myToolbar = (Toolbar) findViewById(R.id.action_bar_default);
-        //setSupportActionBar(myToolbar);
-    //}
 
     @Override
     protected Fragment createFragment() {
--- app/src/main/java/com/rasikagayan/android/viewpagerexample/SingleFragmentActivity.java
+++ app/src/main/java/com/rasikagayan/android/viewpagerexample/SingleFragmentActivity.java
@@ -1,9 +1,11 @@
 package com.rasikagayan.android.viewpagerexample;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
 /**
@@ -12,12 +14,13 @@ import android.support.v7.widget.Toolbar;
 
 // in here we can host any kind of fragment
 // base component
-public abstract class SingleFragmentActivity extends Activity {
+public abstract class SingleFragmentActivity extends AppCompatActivity {
 
     protected abstract Fragment createFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         // this layout will hold any kind of fragment
         // base layout
@@ -25,12 +28,12 @@ public abstract class SingleFragmentActivity extends Activity {
         //Toolbar myToolbar = (Toolbar) findViewById(R.id.action_bar_default);
         //setSupportActionBar(myToolbar);
         FragmentManager fm = getFragmentManager();
-        Fragment fragment = fm.findFragmentById(R.id.list_item_inner_container);
+        Fragment fragment = fm.findFragmentById(R.id.fragment_container);
 
         if (fragment == null) {
             fragment = createFragment();
             fm.beginTransaction()
-                    .add(R.id.list_item_inner_container, fragment)
+                    .add(R.id.fragment_container, fragment)
                     .commit();
         }
 
